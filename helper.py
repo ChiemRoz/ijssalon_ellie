4018579d5ef5aa1d05221002161c73c87a621b16
ChiemRoz <chiemrozema@hotmail.com>
2024-03-20 19:21:33 +0100

    les 10 toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,26 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    bedrag_pp = bedrag / personen
+    return f"Het bedrag per persoon is {bedrag_pp} euro"
+
+def onderstreep(tekst=""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+from boekhouding import inkomsten
+
+def som(dictionary):
+    total = 0
+    for value in dictionary.values():
+        total += value
+    return total
+
+resultaat = som(inkomsten)
+print("Totale inkomsten:", resultaat)
+
+
